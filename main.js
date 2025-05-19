cffbc3eee03f7766c716c1ec43d8732861d761c6
KleversDC <klevers170@gmail.com>
2025-05-19 17:41:51 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,152 @@
+// let age = prompt("Enter Your Age");
+// alert(age);
+
+
+// let num1 = prompt("Enter num 1")
+// let num2 = prompt("Enter num 2")
+// let averge = ((+num1 + +num2)/2);
+// alert(averge);
+
+// let cvadrat = prompt("Enter Pcosk Cvadrat")
+// averge = +cvadrat * 4;
+// alert(averge);
+
+const KMTOMILES = 0.621371;
+let kilometers = prompt("Enter km :");
+let miles = kilometers * KMTOMILES;
+alert(kilometers + " km = " + miles + " mul");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function MakeSomP(a,d)
+// {
+//     for(let i = 0; i<10; i++)
+//     {
+//     document.write("<h>Hello from funtion</h>")
+//     }
+
+// }
+
+// function args()
+// {
+//     console.log (arguments)
+
+// }
+// args(1,2,3,4);
+
+
+
+
+// let array = [1,2,3,4,5,false,"Hi" ,10.44,[1,2,3]];
+// for(let item of array)
+// {
+//     console.log(item)
+//     //break; continue;
+// }
+// let num = Math.round(Math.random(100)*100);
+// alert(num);
+
+
+
+// let a = 10;
+// while(a>0);
+// {
+// console.log(a);
+// a--;
+// }
+// for(let i = 0; i<10; i++)
+// {
+//     console.log(i);
+// }
+// do
+// {
+//     console.log(a);
+// }
+// while(a>0);
+
+// let a = prompt("Enter first numder");
+// let d = prompt("Enter second numder ");
+// console.log(`Sum of ${a} and ${d} is ${+a + b}`);
+
+// a>d? console.log(a):console.log(d);
+
+
+
+
+
+// alert("Hello Wold");
+// let answer = confirm("Hello user");
+// console.log(answer);
+// if(answer)
+// {
+//     alert("Great answer");
+// }
+
+
+
+// console.log("Hello from Console");
+// const  a = 10;
+// console.log(typeof(a));
+// let h ="Hello from console";
+// let c = true;
+// let d = null;
+// let e = undefined;
+// let array = [1,2,3,4,5,false,"Hi" ,10.44,[1,2,3]];
+
+
+// function summ(a,d)
+// {
+//     console.log (`sum of numders :${a} + ${d} = ${a+d}`);
+// }
+
+
+
+
+
+
+
+
+
+
+
+// console.log(typeof(h));
+// console.log(typeof(c));
+// console.log(typeof(d));
+// console.log(typeof(e));
+// console.log(typeof(array));
+// console.log(array[3]);
+// console.log(typeof(summ));
+// console.log(typeof(2**5));
+// console.log(+ '3'!==3);
+// summ(10,33);
